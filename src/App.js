b649fdb075a581b0d3c67492f20c71d85a26021f
KIMOYAHYA <KHAYEMYYAHIA@GMAIL.COM>
2021-09-19 11:27:11 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div style="border:solid 1px black;max-width:100vw">
+
+<h1 className="title red">Your name here</h1>
+
+
+
+<img src="/imageInSrc.jpg" />
+
+<br></br>
+
+<img src="/imageInPublic.jpg" />
+
+</div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
